fabe8b6f36a468914d06c6fd3f4b90c4b37f9110
Danilo <danilo.sl09@aluno.ifsc.edu.br>
2023-04-10 20:35:03 -0300

    v v Changes to be committed:

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const routes = require("./routes");
 const app = express();
+
+
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.listen(3000, () => {
     console.log('Servidor funcionando...')
 
-})
+});
