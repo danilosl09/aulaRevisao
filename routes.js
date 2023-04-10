62ad3eff0664db2996d3cfbab33ff2b92f50e695
Danilo <danilo.sl09@aluno.ifsc.edu.br>
2023-04-10 20:45:53 -0300

    teste3

--- routes.js
+++ routes.js
@@ -2,5 +2,8 @@ const express = require("express");
 
 const routes = express.Router();
 
+let lista = [{"id":1, "nome":"João","idade":20},
+             {"id":2, "nome":"João","idade":20},
+             {"id":3, "nome":"João","idade":20}];
 
-module.exportes = routes
+module.exports = routes;
