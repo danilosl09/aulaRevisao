37d0f11e5ce81c5953d024f24bf6066e4f6f8e14
Danilo <danilo.sl09@aluno.ifsc.edu.br>
2023-04-10 20:51:15 -0300

    teste4

--- routes.js
+++ routes.js
@@ -6,4 +6,8 @@ let lista = [{"id":1, "nome":"João","idade":20},
              {"id":2, "nome":"João","idade":20},
              {"id":3, "nome":"João","idade":20}];
 
+routes.get("/",(req, res) =>{
+    res.status(200).json(lista);
+});
+
 module.exports = routes;
